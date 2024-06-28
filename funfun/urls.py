59cd23b07f96342d1e819e1295735b2e896438e7
seonyo <jsy6535@naver.com>
2024-06-28 17:55:09 +0900

    ✨ [Feat] 수정하기 url 연결

--- funfun/urls.py
+++ funfun/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('list/', views.ItemListView.as_view(), name='item_list'),
     path('create/', views.ItemCreateView, name='item_create'),
-    path('update/<int:pk>', views.ItemUpdateView.as_view(), name='item_update'),
-    path('delete/<int:pk>', views.ItemDeleteView.as_view(), name='item_delete'),
+    path('update/<int:pk>/', views.ItemUpdateView.as_view(), name='item_update'),
+    path('delete/<int:pk>/', views.ItemDeleteView.as_view(), name='item_delete'),
     path('mypage/', views.MypageView.as_view(), name='mypage'),
 ]
