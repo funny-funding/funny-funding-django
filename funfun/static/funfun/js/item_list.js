3deec379b98e34fec30e0de1977c85a2bc431d1f
seonyo <jsy6535@naver.com>
2024-06-29 15:19:11 +0900

    ✨ [Feat] 필터 선택했을 때 css 유지되는 기능 추가

--- funfun/static/funfun/js/item_list.js
+++ funfun/static/funfun/js/item_list.js
@@ -1,6 +1,7 @@
 function selectCategory(selected) {
     // 모든 카테고리 아이템을 가져오기
     let categories = document.querySelectorAll('.category-container ul li');
+
     // 모든 카테고리를 순회하면서 클래스 제거
     categories.forEach(function(category) {
         category.classList.remove('selected-category');
@@ -12,6 +13,24 @@ function selectCategory(selected) {
     currentCategory = selected.getAttribute('data-category-id');
     console.log('Selected Category ID:', currentCategory);
 
-    // 페이지 새로고침 및 카테고리 전달
-    window.location.href = "?category=" + currentCategory;
+    let urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('category', currentCategory);
+    history.replaceState(null, null, '?' + urlParams.toString());
 }
+
+function executeSelectCategoryOnLoad() {
+    let urlParams = new URLSearchParams(window.location.search);
+    let categoryParam = urlParams.get('category');
+
+    if (categoryParam !== null) {
+        let categoryItems = document.querySelectorAll('.category-container ul li');
+        categoryItems.forEach(function(item) {
+            let categoryId = item.getAttribute('data-category-id');
+            if (categoryId === categoryParam) {
+                selectCategory(item);
+            }
+        });
+    }
+}
+
+window.onload = executeSelectCategoryOnLoad;
