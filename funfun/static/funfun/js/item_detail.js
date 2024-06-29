7f9e81eb32d31eb47df01af4a82497f54ef45178
haeunNoh06 <okhagrace06@naver.com>
2024-06-29 16:55:53 +0900

    :bug: [refactor] d-day 집계 오류 해결

--- funfun/static/funfun/js/item_detail.js
+++ funfun/static/funfun/js/item_detail.js
@@ -1,3 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // 펀딩 금액 달성 퍼센트 구하기
+    let goalMoney = parseInt(document.querySelector('.goal-money').innerText);
+    let successMoney = parseInt(document.querySelector('.success-money').innerText);
+    let currentPercent = (successMoney / goalMoney) * 100;
+    document.querySelector('.current-percent').innerText = currentPercent.toFixed(2);
+
+    // D-Day 계산
+    let endDate = new Date(itemEndPeriod);
+    console.log("endDate: " + endDate);
+    let today = new Date();
+    console.log("today: " + today);
+    let timeDiff = endDate - today;
+    console.log("timeDiff: " + timeDiff);
+    let daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    let dDayText = document.getElementById('d-day-text');
+    let dDayElement = document.getElementById('d-day');
+    let dDayPrefixElement = document.getElementById('d-day-prefix');
+    let dDaySuffixElement = document.getElementById('d-day-suffix');
+    console.log("daysDiff: " + daysDiff);
+
+    if (daysDiff > 0) {
+        dDayElement.innerText = daysDiff;
+    } else if (daysDiff === 0) {
+        dDayElement.innerText = "Day";
+    } else {
+        dDayText.innerText = "";
+        dDayElement.innerText = "";
+        dDayPrefixElement.innerText = "";
+        dDaySuffixElement.innerText = "펀딩이 마감되었어요";
+
+        // 펀딩이 마감되었을 때 버튼의 onclick 이벤트 제거
+        let fundingButton = document.getElementById('participate-funding-btn');
+        fundingButton.innerText = "펀딩에 참여할 수 없어요";
+        fundingButton.onclick = null;
+        fundingButton.style.cursor = "not-allowed"; // 버튼 스타일 변경
+    }
+});
+
 function openEditPopup(commentId, content) {
     let popup = document.getElementById('edit-comment-popup');
     let form = document.getElementById('edit-comment-form');
@@ -36,39 +76,3 @@ window.onclick = function (event) {
         closeFundingPopup();
     }
 }
-
-// 펀딩 금액 달성 퍼센트 구하기
-document.addEventListener('DOMContentLoaded', function () {
-    let goalMoney = parseInt(document.querySelector('.goal-money').innerText);
-    let successMoney = parseInt(document.querySelector('.success-money').innerText);
-    let currentPercent = (successMoney / goalMoney) * 100;
-    document.querySelector('.current-percent').innerText = currentPercent.toFixed(2);
-
-    // D-Day 계산
-    let endDateString = "{{ item.end_period|date:'Y-m-d' }}";
-    let endDate = new Date(endDateString);
-    let today = new Date();
-    let timeDiff = endDate - today;
-    let daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
-
-    let dDayText = document.getElementById('d-day-text');
-    let dDayElement = document.getElementById('d-day');
-    let dDayPrefixElement = document.getElementById('d-day-prefix');
-    let dDaySuffixElement = document.getElementById('d-day-suffix');
-
-    if (daysDiff > 0) {
-        dDayElement.innerText = daysDiff;
-    } else if (daysDiff === 0) {
-        dDayElement.innerText = "D-Day";
-    } else {
-        dDayText.innerText = "";
-        dDayElement.innerText = "";
-        dDayPrefixElement.innerText = "";
-        dDaySuffixElement.innerText = "펀딩이 마감되었어요";
-        // 펀딩이 마감되었을 때 버튼의 onclick 이벤트 제거
-        let fundingButton = document.getElementById('participate-funding-btn');
-        fundingButton.innerText = "펀딩에 참여할 수 없어요"
-        fundingButton.onclick = null;
-        fundingButton.style.cursor = "not-allowed"; // 버튼 스타일 변경
-    }
-});
